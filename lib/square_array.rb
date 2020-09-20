8d8183ed97b56a53de756feb2e19c695f9ece25a
Matt Bugay <mjbugay@yahoo.com>
2020-09-20 22:46:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   new_numbers = []
   i = 0
   while i < numbers.length() do
-    new_numbers.push(numbers)
+    new_numbers.push(numbers[i] * numbers[i])
     i += 1
   end
   new_numbers
