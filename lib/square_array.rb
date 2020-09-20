3415f0e34da7c7f542c2d7558333da3e120a691e
Matt Bugay <mjbugay@yahoo.com>
2020-09-20 22:39:39 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
-def square_array(numbers[i])
+def square_array(numbers)
   new_numbers = []
-  while i < numbers.length do
+  count = 0
+  while count < numbers.length do
     new_numbers << (numbers[i] ** 2)
   end
   new_numbers
